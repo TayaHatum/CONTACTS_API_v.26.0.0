d908d51fa18c32024bf13e49a5beec267f88169a
TayaHatum <21taya21@gmail.com>
2021-03-01 20:21:10 +0200

    added okHttptest

--- src/test/java/com/telran/apachetest/ApacheTest.java
+++ src/test/java/com/telran/apachetest/ApacheTest.java
@@ -1,13 +1,21 @@
 package com.telran.apachetest;
 
+import com.google.gson.Gson;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonParser;
+import com.telran.dto.AuthRequestDto;
+import com.telran.dto.AuthResponseDto;
+import com.telran.dto.ErrorDto;
+import org.apache.http.HttpResponse;
 import org.apache.http.client.fluent.Request;
 import org.apache.http.client.fluent.Response;
 import org.apache.http.entity.ContentType;
 import org.testng.annotations.Test;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 public class ApacheTest {
 
@@ -18,24 +26,64 @@ public class ApacheTest {
 
         Response response = Request.Post("https://contacts-telran.herokuapp.com/api/login")
                 .bodyString("{\n" +
-                        "  \"email\": \""+email+"\",\n" +
-                        "  \"password\": \""+password+"\"\n" +
+                        "  \"email\": \"" + email + "\",\n" +
+                        "  \"password\": \"" + password + "\"\n" +
                         "}", ContentType.APPLICATION_JSON)
                 .execute();
+        System.out.println(response);
+
 
         String responseJson = response.returnContent()
                 .asString();
 
-       System.out.println(responseJson);
+        System.out.println(responseJson);
         System.out.println("***********");
 
         JsonElement element = JsonParser.parseString(responseJson);
+
         JsonElement token = element.getAsJsonObject().get("token");
         System.out.println(token.getAsString());
 
     }
 
+    @Test
+    public void testLogin() throws IOException {
+        AuthRequestDto requestDto = AuthRequestDto.builder()
+                .email("john9090@mail.com")
+                .password("Aa12345~").build();
+        Gson gson = new Gson();
+        Response response = Request.Post("https://contacts-telran.herokuapp.com/api/login")
+                .bodyString(gson.toJson(requestDto), ContentType.APPLICATION_JSON)
+                .execute();
+        String responseJson = response.returnContent().asString();
+        AuthResponseDto responseDto = gson.fromJson(responseJson, AuthResponseDto.class);
+        System.out.println(responseDto.getToken());
+    }
 
+    @Test
+    public void testLogin1() throws IOException {
+        AuthRequestDto requestDto = AuthRequestDto.builder()
+                .email("john9090@mail.com")
+                .password("a12345~").build();
+        Gson gson = new Gson();
+        Response response = Request.Post("https://contacts-telran.herokuapp.com/api/login")
+                .bodyString(gson.toJson(requestDto), ContentType.APPLICATION_JSON)
+                .execute();
+        HttpResponse httpresponse = response.returnResponse();
+        System.out.println(httpresponse);
+        System.out.println(httpresponse.getStatusLine().getStatusCode());
 
-
+        InputStream is = httpresponse.getEntity().getContent();
+        BufferedReader br= new BufferedReader(new InputStreamReader(is));
+        String line;
+        StringBuilder sb = new StringBuilder();
+        while ((line= br.readLine())!=null){
+            sb.append(line);
+        }
+        System.out.println(sb.toString());
+        ErrorDto errorDto = gson.fromJson(sb.toString(),ErrorDto.class);
+        System.out.println(errorDto.getCode());
+        System.out.println(errorDto.getMessage());
+    }
 }
+//eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6ImpvaG45MDkwQG1haWwuY29tIn0.uFp-ZueoxtelSBWIrrsl7SmMAKUNBmjQNj33Zh2ik6w
--- src/test/java/com/telran/testgson/Person.java
+++ src/test/java/com/telran/testgson/Person.java
@@ -7,6 +7,7 @@ import lombok.*;
 @Setter
 @Getter
 @ToString
+
 public class Person {
     String name;
     int age;
